0062504f0f75beecd3b26b0eaf03c1fef032b150
Malgorzata <malg.sem@gmail.com>
2020-10-25 14:52:12 +0100

    add number and page

--- yarnmarket/spiders/kokonki_himalaya_spider.py
+++ yarnmarket/spiders/kokonki_himalaya_spider.py
@@ -1,24 +1,47 @@
 import scrapy
-#TODO: Add what page
-#TODO: Automate pagination
-#TODO: database
+import re
+
+
+# TODO: Add what page
+# TODO: Automate pagination
+# TODO: database
 
 class KokonkiHimalayaSpider(scrapy.Spider):
     name = "kokonkiHimalaya"
     start_urls = [
         'https://kokonki.pl/wloczka-himalaya-dolphin-baby/1',
-        'https://kokonki.pl/wloczka-himalaya-dolphin-baby/2/',
-        'https://miladruciarnia.pl/pl/c/Doplhin-Baby/99',
-        'https://amicrafts.pl/pl/c/Dolphin-Baby/380',
-        'https://amicrafts.pl/pl/c/Dolphin-Baby/380/2'
+        # 'https://kokonki.pl/wloczka-himalaya-dolphin-baby/2/',
+        # 'https://miladruciarnia.pl/pl/c/Doplhin-Baby/99',
+        # 'https://amicrafts.pl/pl/c/Dolphin-Baby/380',
+        # 'https://amicrafts.pl/pl/c/Dolphin-Baby/380/2'
     ]
-    def parse(self,response):
+
+    def parse(self, response):
         for post in response.css('div.product.s-grid-3.product-main-wrap'):
-            yield{
-               'name': post.css('.productname::text').get(),
-               'price': post.css('.price em::text').get(),
-               'availability': post.css('.buttons.f-row fieldset button span::text').get()
+            name = post.css('.productname::text').get()
+            price = float(post.css('.price em::text').get().replace('\xa0zł', '').replace(',', '.'))
+            page = response.css('.link-logo img::attr(alt)').get()
+            # get the yarn number
+            if re.search("3\d\d", name) is not None:
+                number = re.search("3\d\d", name).group(0)
+            else:
+                number = None
+
+            # get the yarn availability
+            if post.css('.buttons.f-row fieldset button span::text').get() == 'Do koszyka':
+                availability = True
+            else:
+                availability = False
+
+            yield {
+                'name': name,
+                'price': price,
+                'availability': availability,
+                'number': number,
+                'page': page
             }
+        print(page)
+
         # next_page = response.css('a.next-posts-link::attr(href)').get()
         # if next_page is not None:
         #     next_page = response.urljoin(next_page)
